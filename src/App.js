bf6a83701e8325435f38f3d173ca7b8d2fba717a
abhisheksubba.py@gmail.com <abhisheksubba.py@gmail.com>
2023-06-20 18:50:27 +0530

    github

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename="/">
         <NavBar />
         <Routes>
           <Route exact path="/" element={<Home />} />
